66bb17cee33f0f275f0d6df63984004740cfe79c
Ishaan Pandey <ishaanmaybe@gmail.com>
2026-01-25 01:03:07 +0530

    Handling Url's

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import http from "http";
 import fs from "fs";
+import url from "url";
 
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}: New Req Recieved \n`;
-  fs.appendFile("log/txt", log, (err, data) => {
-    res.end("Hello from sever");
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()}: ${req.url} New Req Recieved \n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        const username = myUrl.query.myname;
+        res.end(`Hi ${username}`);
+        break;
+      default:
+        res.end("404 not found");
+    }
   });
 });
 
